f7e3a91e01dc6c746dc001cff6265a621de32b79
attorneycat <56152557+attorneycat@users.noreply.github.com>
2019-12-19 19:39:14 +0700

    Update static.config.js

--- static.config.js
+++ static.config.js
@@ -6,6 +6,21 @@ import { renderToString } from "react-dom/server";
 
 import postsJson from "./src/pages/blog/components/posts.json";
 
+function tawkfunction(){
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+  (function() {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/5dfb441043be710e1d22c6de/default";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+}
+
+
 export default {
   siteRoot: "https://usaatty.com",
   paths: {
@@ -71,19 +86,7 @@ export default {
         ></script>
 
         <script type="text/javascript">
-          {() => {
-            var Tawk_API = Tawk_API || {},
-              Tawk_LoadStart = new Date();
-            (function() {
-              var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-              s1.async = true;
-              s1.src = "https://embed.tawk.to/5dfb441043be710e1d22c6de/default";
-              s1.charset = "UTF-8";
-              s1.setAttribute("crossorigin", "*");
-              s0.parentNode.insertBefore(s1, s0);
-            });
-          }}
+          {tawkfunction}
         </script>
       </Html>
     );
